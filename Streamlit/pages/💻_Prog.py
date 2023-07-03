09c19015e9176910a5681913bf0dd8423e50fb26
GermanPaul12 <german.paul@outlook.de>
2023-07-03 20:55:26 +0200

    update prog

--- Streamlit/pages/💻_Prog.py
+++ Streamlit/pages/💻_Prog.py
@@ -5,5 +5,15 @@ st.title("Welcome to the 💻 Advanced Programming Page")
 
 st.write("---")
 
-st.subheader("Use it yourself with:")
+st.subheader("Use the submodule yourself with:")
+st.code("pip install datawhispers", language="bash")
 st.code("from datawhispers import advancedProg as ap")
+
+st.write("---")
+
+st.subheader("Show MNIST NUmber from csv-file")
+
+uploaded_file = st.file_uploader(label="upload your csv-file")
+if uploaded_file is not None:
+    st.write(ap.show_mnist_from_file(uploaded_file))
+    
